1586a1e9d1ed7d5399fb29a8b0a9d8435cfc8192
JCampbell-6 <jordancampbell06@gmail.com>
2023-11-10 04:51:25 -0400

    Fixed Q1 so it worked correctly

--- q1_timesheet.js
+++ q1_timesheet.js
@@ -9,34 +9,58 @@ Title: PROG1700 Assignment 3- Q1 Timesheet
 // DO NOT EDIT: The main function to house our program code 
 function main()
 {
-    //Stores User inputed hours worked.
+//Stores User inputed hours worked.
 const hoursWorked = [];
     //Stores Day 1 through Day 5 (Mon-Fri).
 const daysOfWorkWeek = ["Day 1", "Day 2", "Day 3", "Day 4", "Day 5"]
 
-    //Prompts the user to input amount of hours worked for each day of the week.
+//Prompts the user to input amount of hours worked for each day of the week.
 for (let i = 0; i < daysOfWorkWeek.length; i++){
     let hours = readlineSync.questionFloat(`Enter hours worked on ${daysOfWorkWeek[i]}: `);
     hoursWorked.push(hours);
 }
 console.log("--------------------------------------------------------------------------------------------------");
-    //mostHoursWorked Uses Math.max to calculate the largest number in the hoursWorked array
-    //mostWorkedDays uses the filter function to 
-console.log("--------------------------------------------------------------------------------------------------");
 
-    //mostHoursWorked Uses Math.max to calculate the largest number in the hoursWorked array
-    //mostWorkedDays uses the filter function to 
+//mostHoursWorked Uses Math.max to calculate the largest number in the hoursWorked array
+//mostWorkedDays uses the filter function to 
 let mostHoursWorked = Math.max(...hoursWorked);
 let mostWorkedDays = daysOfWorkWeek.filter((day, index) => hoursWorked[index] === mostHoursWorked);
 
-    //Prints the days that you worked the most hours
+//Prints the days that you worked the most hours
 if (mostWorkedDays.length === 1){
     console.log(`The most hours worked was on: ${mostWorkedDays[0]} when you worked ${mostHoursWorked} hours`);
 }else{
     console.log(`The most hours worked was on: ${mostWorkedDays.join(', ')} when you worked ${mostHoursWorked} hours each day`)
 }
+console.log("--------------------------------------------------------------------------------------------------");
 
+//For loop that calculate the sum of all numbers in the array and then prints it
+let sum = 0
+for (let i = 0; i <= hoursWorked[i]; i++){
+    sum = sum + hoursWorked[i]
 }
+console.log("The total number of hours worked was: " + sum);
+console.log("The average number of hours worked each day was: " + (sum / hoursWorked.length));
+
+console.log("--------------------------------------------------------------------------------------------------");
+
+let insufficientHours = [];
+for (let i = 0; i < daysOfWorkWeek.length; i++) {
+// Check if the hours worked on the current day is less than 7
+    if (hoursWorked[i] < 7) {
+// If so, add the day to the insufficientHoursDays array
+        insufficientHours.push(daysOfWorkWeek[i]);
+    }
+}
+
+// Check if there are any days with insufficient hours
+if (insufficientHours.length > 0) {
+// If there are, print a message with the list of days
+    console.log("Days you slacked off (i.e. worked less than 7 hours): " + insufficientHours.join(', '));
+}
+}
+
+
 
 // DO NOT EDIT: Trigger our main function to launch the program
 if (require.main === module)
